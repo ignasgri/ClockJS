0e015eeeed7f5716ecb6913ad779400e8da29123
Ignas Grigaravicius <kickeris@gmail.com>
2017-10-15 12:11:42 +0100

    graphic element added for sec

--- sketch.js
+++ sketch.js
@@ -1,11 +1,18 @@
 function setup(){
     createCanvas(400, 400);
+    angleMode(DEGREES);
 }
 function draw(){
     background(0);
     let hr = hour();
     let min = minute();
     let sec = second();
+
+    strokeWeight(4);
+    stroke(255, 100, 150);
+    noFill();
+    let end = map(mouseX, 0, width,0 ,360);
+    arc(200, 200, 300, 300, 0, end);
 
     fill(255);
     noStroke();
