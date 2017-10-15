334cc4ac78d0ae3ddfdb3c5359ebd809f78bd9f1
Ignas Grigaravicius <kickeris@gmail.com>
2017-10-15 12:35:39 +0100

    graphic element for min, hr added

--- sketch.js
+++ sketch.js
@@ -11,10 +11,15 @@ function draw(){
     strokeWeight(4);
     stroke(255, 100, 150);
     noFill();
-    let end = map(mouseX, 0, width,0 ,360);
-    arc(200, 200, 300, 300, 0, end);
+    let endsec = map(sec, 0, 60,0 ,360);
+    arc(200, 200, 300, 300, 0, endsec);
 
-    fill(255);
-    noStroke();
-    text(hr + ':' + min + ':' + sec, 10, 200);
+    let endmin = map(min, 0, 60,0 ,360);
+    arc(200, 200, 280, 280, 0, endmin);
+
+    let endhr = map(hr, 0, 24,0 ,360);
+    arc(200, 200, 260, 260, 0, endhr);
+    // fill(255);
+    // noStroke();
+    // text(hr + ':' + min + ':' + sec, 10, 200);
 }
