002bec1a053105587f4daa15faf11fffbb8875f7
Ignas Grigaravicius <kickeris@gmail.com>
2017-10-15 20:20:32 +0100

    clock rotated, set to 12 hour clock, added colors

--- sketch.js
+++ sketch.js
@@ -4,21 +4,26 @@ function setup(){
 }
 function draw(){
     background(0);
+    translate(200, 200);
+    rotate(-90);
+
     let hr = hour();
     let min = minute();
     let sec = second();
 
-    strokeWeight(4);
+    strokeWeight(8);
     stroke(255, 100, 150);
     noFill();
     let endsec = map(sec, 0, 60,0 ,360);
-    arc(200, 200, 300, 300, 0, endsec);
+    arc(0, 0, 300, 300, 0, endsec);
 
+    stroke(150, 100, 255);
     let endmin = map(min, 0, 60,0 ,360);
-    arc(200, 200, 280, 280, 0, endmin);
+    arc(0, 0, 280, 280, 0, endmin);
 
-    let endhr = map(hr, 0, 24,0 ,360);
-    arc(200, 200, 260, 260, 0, endhr);
+    stroke(150, 255, 100);
+    let endhr = map(hr % 12, 0, 12, 0 ,360);
+    arc(0, 0, 260, 260, 0, endhr);
     // fill(255);
     // noStroke();
     // text(hr + ':' + min + ':' + sec, 10, 200);
