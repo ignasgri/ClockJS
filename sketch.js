5a3d27529c28f0dd0aa3ae2fa48f386f0254d6e0
Ignas Grigaravicius <kickeris@gmail.com>
2017-10-15 20:37:31 +0100

    arrows added

--- sketch.js
+++ sketch.js
@@ -16,14 +16,36 @@ function draw(){
     noFill();
     let endsec = map(sec, 0, 60,0 ,360);
     arc(0, 0, 300, 300, 0, endsec);
+    
+    push();
+    rotate(endsec);
+    stroke(255, 100, 150);
+    line(0, 0, 100, 0);
+    pop();
 
     stroke(150, 100, 255);
     let endmin = map(min, 0, 60,0 ,360);
     arc(0, 0, 280, 280, 0, endmin);
 
+    push();
+    rotate(endmin);
+    stroke(150, 100, 255);
+    line(0, 0, 75, 0);
+    pop();
+
     stroke(150, 255, 100);
     let endhr = map(hr % 12, 0, 12, 0 ,360);
     arc(0, 0, 260, 260, 0, endhr);
+
+    push();
+    rotate(endhr);
+    stroke(150, 255, 100);
+    line(0, 0, 50, 0);
+    pop();
+
+    strokeWeight(2);
+    stroke(255);
+    ellipse(0, 0, 10, 10);
     // fill(255);
     // noStroke();
     // text(hr + ':' + min + ':' + sec, 10, 200);
