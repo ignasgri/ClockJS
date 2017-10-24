cb5fabb9645d2aed24070eed93c4fbaeaff455be
Ignas Grigaravicius <kickeris@gmail.com>
2017-10-24 18:42:11 +0100

    clock design added, background image added

--- sketch.js
+++ sketch.js
@@ -1,51 +1,60 @@
 function setup(){
-    createCanvas(400, 400);
+    createCanvas(180, 180);
     angleMode(DEGREES);
 }
 function draw(){
-    background(0);
-    translate(200, 200);
+    background(0, 128, 0, 0);
+    
+    translate(90,90);
     rotate(-90);
 
     let hr = hour();
     let min = minute();
     let sec = second();
-
-    strokeWeight(8);
+    
+    strokeWeight(4);
     stroke(255, 100, 150);
     noFill();
     let endsec = map(sec, 0, 60,0 ,360);
-    arc(0, 0, 300, 300, 0, endsec);
+    arc(0, 0, 150, 150, 0, endsec);
+
     
-    push();
-    rotate(endsec);
-    stroke(255, 100, 150);
-    line(0, 0, 100, 0);
-    pop();
+    
+   
 
     stroke(150, 100, 255);
     let endmin = map(min, 0, 60,0 ,360);
-    arc(0, 0, 280, 280, 0, endmin);
+    arc(0, 0, 140, 140, 0, endmin);
 
     push();
     rotate(endmin);
     stroke(150, 100, 255);
-    line(0, 0, 75, 0);
+    line(0, 0, 35, 0);
     pop();
 
     stroke(150, 255, 100);
     let endhr = map(hr % 12, 0, 12, 0 ,360);
-    arc(0, 0, 260, 260, 0, endhr);
+    arc(0, 0, 130, 130, 0, endhr);
 
     push();
     rotate(endhr);
     stroke(150, 255, 100);
-    line(0, 0, 50, 0);
+    line(0, 0, 30, 0);
     pop();
 
-    strokeWeight(2);
+     push();
+    rotate(endsec);
+    strokeWeight(1);
     stroke(255);
-    ellipse(0, 0, 10, 10);
+    line(2, 0, 45, 0);
+    pop();
+
+    strokeWeight(1);
+    stroke(255);
+    fill(0);
+    ellipse(0, 0, 5, 5);
+    
+    
     // fill(255);
     // noStroke();
     // text(hr + ':' + min + ':' + sec, 10, 200);
